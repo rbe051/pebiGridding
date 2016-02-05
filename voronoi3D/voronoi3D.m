286631380caebd3c4c32bfe3f376c471f99c1a6e
Runar Berge <runarlb@stud.ntnu.no>
2016-02-05 11:47:03 +0100

    Made the voronoi3D code visually appealing

--- voronoi3D/voronoi3D.m
+++ voronoi3D/voronoi3D.m
@@ -19,48 +19,23 @@ function [G] = voronoi3D(pts, bound)
         pts = [pts; mirror(pts, bound(K(i,1),:),normals(i,:))];
     end
     
-    %figure
-    %hold on
-    %plot3(bound(:,1), bound(:,2), bound(:,3),'.')
-    %plot3(pts(:,1),pts(:,2),pts(:,3),'m.')
-    %plot3(pts(:,1),pts(:,2),pts(:,3),'ro')
-    
+    % Generate voronoi grid
     [V, C] = voronoin(pts);
-  
-    
+
     % Find auxillary cells
     remove = false(numel(C),1);
-    figure();
-    hold on
-    legendStr = [];
     for i = 1:numel(C)
         avgCell = sum(V(C{i},:),1)/size(C{i},2);
         if any(isinf(avgCell)) || isnan(pointLocation(dt, avgCell));
             remove(i) = true;
-        else
-            plot3(avgCell(:,1), avgCell(:,2),avgCell(:,3),'.','markersize', 30);
-            legendStr = [legendStr, i];
         end
     end
-    legend(num2str(legendStr(:)))
-    for i = 1:numel(C)
-       VertCell = V(C{i},:); 
-       if all(C{i}~=1) && ~remove(i)
-            KVert = convhulln(VertCell);
-            patch('Vertices',VertCell,'Faces',KVert,'FaceColor',[0,0,i/numel(C)],'FaceAlpha',0.5) 
-       end
-    end
-    xlabel('x')
-    ylabel('y')
-    zlabel('z')
-    hold on
     
-    G = voronoi2mrst(V,C, remove);
-
-
-
+    % Convert to mrst Grid-structure
+    G = voronoi2mrst(V,C, remove, mfilename);
 
 end
+
 
 function r = remParPlanes(x0, n)
     r = false(size(x0,1),1);
