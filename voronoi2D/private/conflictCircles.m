87754258a814288eeeb9d70fc410315fc062f856
Runar Berge <runarlb@stud.ntnu.no>
2016-04-22 09:03:21 +0200

    Updated errortolerance in conflictCircles.m

--- voronoi2D/private/conflictCircles.m
+++ voronoi2D/private/conflictCircles.m
@@ -1,13 +1,13 @@
 function [I] = conflictCircles(Pts, CC, CR)
-    TOL = 10*eps;
+    TOL = 100*eps;
 
     nc = size(CC,1);
     np = size(Pts,1);
-    removed = zeros(np,1);
-    CRSqr = CR.^2;
+
+    %CRSqr = CR.^2;
     I = cell(numel(CR),1);
     for i = 1:nc
-        distSqr = sum((repmat(CC(i,:),np,1)-Pts).^2,2);
-        I{i} = find(distSqr<CRSqr(i)-10*eps);
-    end                
+        dist = sqrt(sum((repmat(CC(i,:),np,1)-Pts).^2,2));
+        I{i} = find(dist<CR(i)-CR(i)*TOL);
+    end
 end
