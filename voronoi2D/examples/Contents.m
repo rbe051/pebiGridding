228c78bb744e6ca4345b69988f4a96639ffa2d43
Runar Berge <runarlb@stud.ntnu.no>
2016-04-22 15:11:13 +0200

    Added two more examples.
    The examples show step-by-step how one can create fault seeds and
    well seeds.

--- voronoi2D/examples/Contents.m
+++ voronoi2D/examples/Contents.m
@@ -1,6 +1,10 @@
 % Example grids for voronoi-2D
 %
 % Files
+%   explainFaultAlgo    - Goes through all steps needed to create a grid
+%                         conforming to a fault.
+%   explainWellAlgo     - Goes through all steps needed to create a grid
+%                         conforming to a well
 %   grid_2_5D.m         - Create a 2.5D grid with two faults and three
 %                         wells
 %   intWellAndFaults    - Create a grid with one well and several faults
---
+++ voronoi2D/examples/explainWellAlgo.m
@@ -0,0 +1,61 @@
+% This script goes trough the most important details about how to algorithm
+% creates a grid conforming to wells.
+% It does not cover the details, nor does it cover intersecting wells, but
+% it will hopefully enlighten persons that whish to implement their own
+% algorithms conforming to wells. 
+
+% Copyright (C) 2016 Runar Lie Berge. See COPYRIGHT.TXT for details.
+
+
+%% Create a Well
+% We create a circular well. This is mostly to demonstrate the flexibillity
+% of the gridding method. 
+dWell = 0.1;                         % Grid cell size
+n     = 20;                          % Number of line-segments that approximate the well
+dT    = 2*pi/20;
+theta = 2*pi*(0:1/n:1 -1/n)';       
+well  = repmat([0.5,0.5],n,1) ...
+          + 0.3*[cos(theta), sin(theta)];
+
+%% Plot well
+figure()
+hold on
+plot([well(:,1);well(1,1)], [well(:,2);well(1,2)]);
+axis equal tight
+axis([0.1,0.9,0.1,0.9])
+%% Interpolate fault
+% We interpolate the well with a set of equidistant points
+linesDist = sqrt(sum(diff(well,[],1).^2,2)); % Total length of each line segment
+linesDist = [0; linesDist];                  % Add starting points
+cumDist   = cumsum(linesDist);               % Find cummulative distant to each segment
+dt        = cumDist(end)/...                 % Make sure the end point is included
+              ceil(cumDist(end)/dWell);
+newPtsT   = 0:dt:cumDist(end);               % interpolation points
+wPts   = interp1(cumDist, well, newPtsT);    % well seeds
+
+%% Plot interpolation:
+yellow = [0.9290    0.6940    0.1250];
+blue   = [0    0.4470    0.7410];
+plot(wPts(:,1),wPts(:,2),'.','color',blue,'markersize',15);
+
+%% Cartesian background-grid
+[X,Y]   = meshgrid(0:dWell:1);
+resPts = [X(:),Y(:)];
+removed = any(pdist2(resPts,wPts)<dWell,2);  % Remove any resPts that are 
+resPts = resPts(~removed,:);                 % to close to a well seed
+
+plot(resPts(:,1), resPts(:,2),'.k','markersize',15);
+
+%% Create full grid
+Gt = triangleGrid([wPts;resPts]);
+G  = pebi(Gt);
+figure()
+plotGrid(G,'facecolor','none')
+axis equal tight
+axis([0.1,0.9,0.1,0.9])
+
+
+
+
+
+
